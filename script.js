9730ee2bd8cac88596a4b3adca9a94b89ad91414
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-03-30 14:01:18 +0530

    updated js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let a = 2
-let b = 3
-let c = 7
-console.log(a+b-c)
+// question 1
+// const product = {
+//     name : "Parker jotter standard ball pen",
+//     ratings : 7000,
+//     price : 270,
+//     discount : 5,
+// }
+
+// console.log(product);
+
+// question 2
+const profile = {
+    username : "@Rishab_1024",
+    followers :4321,
+    following :1234,
+    posts : 123,
+    isfollow : true,
+}
+console.log(profile)
